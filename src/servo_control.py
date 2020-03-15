e2893aba0bd53213f2d4234b36782b6cd9953be4
Simon <simon.szalai@gmail.com>
2020-03-15 13:31:32 +0100

    s3 upload and parallel movement

--- src/servo_control.py
+++ src/servo_control.py
@@ -1,11 +1,11 @@
 import os
-import re
-import pigpio
 import time
 import math
-from pathlib import Path
+import pigpio
+import concurrent.futures
 from datetime import datetime
 from video import Camera
+from storage import Storage
 
 """
 GPIO 16: SERVO0, (L)2400-(R)550 // 550 = -90deg, 1450 = 0deg, 2350 = +90deg
@@ -18,40 +18,59 @@ class ServoControl:
     def __init__(self):
         self.pi = pigpio.pi()
         self.camera = Camera()
+        self.storage = Storage()
         self.servos = (16, 20, 21)
-        self.current_set_path = self.create_next_folder()
+        self.current_set_path = self.storage.create_next_folder()
         self.start_positions = {
             self.servos[0]: 1425,
             self.servos[1]: 500,
             self.servos[2]: 1800
         }
         self.speeds = {
-            "slow": 25,
+            "dataset": 50,
             "fast": 500
         }
 
-    def execute_commands(self, commands):
+    def execute_commands_series(self, commands):
         for cmd in commands:
-            servo, end_position = cmd[0], cmd[1]
-            try:
-                speed = self.speeds[cmd[2]]
-            except IndexError:
-                speed = self.speeds["fast"]
-            self.move_arm(servo=servo, start=self.start_positions[servo], end=end_position, speed=speed)
+            self.move_arm(cmd=cmd)
+
+    def execute_commands_parallel(self, commands):
+        with concurrent.futures.ThreadPoolExecutor(max_workers=len(commands)) as executor:
+            for cmd in commands:
+                executor.submit(self.move_arm, cmd=cmd)
 
     def init_arm_for_recording(self):
-        self.execute_commands(((self.servos[0], 2200), (self.servos[2], 1810), (self.servos[1], 1200)))
+        print("Moving arm to initial position...")
+        self.execute_commands_series(((self.servos[2], 1810),))
+        self.execute_commands_parallel(((self.servos[0], 2200), (self.servos[1], 1200)))
+        print("Arm in initial position!")
 
     def reset_arm(self):
-        self.execute_commands(((self.servos[0], 1425), (self.servos[1], 500)))
+        self.execute_commands_parallel(((self.servos[0], 1425), (self.servos[1], 500)))
 
     def do_recording(self):
-        self.camera.start(path=os.path.join(self.current_set_path, datetime.now().strftime("%d.%m.%Y_%H:%M:%S") + ".h264"))
-        self.execute_commands([(self.servos[0], 800, "slow")])
+        video_path = os.path.join(self.current_set_path, datetime.now().strftime("%d.%m.%Y_%H:%M:%S") + ".h264")
+        self.camera.start(path=video_path)
+        self.execute_commands_series([(self.servos[0], 800, "dataset")])
         self.camera.stop()
-        self.init_arm_for_recording()
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            executor.submit(self.storage.upload, video_path)
+            executor.submit(self.init_arm_for_recording)
 
-    def move_arm(self, servo, start, end, speed=1, freq=50, dataset_recording=False):
+    def move_arm(self, cmd):
+        servo, end = cmd[0], cmd[1]
+        start = self.start_positions[servo]
+        dataset_recording = False
+        freq = 50
+
+        try:
+            speed = self.speeds[cmd[2]]
+            if cmd[2] == "dataset":
+                dataset_recording = True
+        except IndexError:
+            speed = self.speeds["fast"]
+
         delta_angle = end - start
         duration = abs(delta_angle) / speed
         steps = abs(freq * duration)
@@ -77,38 +96,6 @@ class ServoControl:
                 time.sleep(sleep_length)
 
         self.start_positions[servo] = trajectory[-1]
-
-    def create_next_folder(self):
-        recordings_path = os.path.join(Path().parent.absolute(), "recordings")
-
-        # List contents of recordings folder or create it if it does not exist
-        folder_names = []
-        try:
-            folder_names = [f.name for f in os.scandir(recordings_path) if f.is_dir()]
-        except FileNotFoundError:
-            os.mkdir(recordings_path)
-            next_folder = os.path.join(recordings_path, "1")
-
-        # Sort folder names
-        def sorted_alphanumeric(data):
-            convert = lambda text: int(text) if text.isdigit() else text.lower()
-            alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
-            return sorted(data, key=alphanum_key)
-        folder_names = sorted_alphanumeric(folder_names)
-
-        # Check if there are any folders in recordings and initialize a folder with number 1 if not
-        try:
-            next_folder = os.path.join(recordings_path, folder_names[-1])
-        except IndexError:
-            next_folder = os.path.join(recordings_path, "1")
-            os.mkdir(next_folder)
-
-        # Check if last folder contains any items and create new folder if it does
-        if len(os.listdir(next_folder)) != 0:
-            next_folder = os.path.join(recordings_path, str(int(folder_names[-1]) + 1))
-            os.mkdir(next_folder)
-
-        return next_folder
 
     def close(self):
         if self.camera.camera.recording:
---
+++ src/storage.py
@@ -0,0 +1,49 @@
+import os
+import re
+import boto3
+from pathlib import Path
+
+
+class Storage:
+    def __init__(self):
+        self.s3 = boto3.resource("s3")
+
+    def upload(self, path):
+        print(f"Uploading {path}...\n")
+        filename = os.path.basename(path)
+        dir_tree = os.path.dirname(path)
+        parent_folder = os.path.basename(dir_tree)
+        self.s3.Bucket("sorterbot-training-videos").upload_file(path, os.path.join(parent_folder, filename))
+        print("Upload completed!")
+
+    def create_next_folder(self):
+        recordings_path = os.path.join(Path().parent.absolute(), "recordings")
+
+        # List contents of recordings folder or create it in case it does not exist
+        try:
+            folder_names = [f.name for f in os.scandir(recordings_path) if f.is_dir()]
+        except FileNotFoundError:
+            folder_names = []
+            os.mkdir(recordings_path)
+            next_folder = os.path.join(recordings_path, "1")
+
+        # Nauturally sort folder names
+        def sorted_alphanumeric(data):
+            convert = lambda text: int(text) if text.isdigit() else text.lower()
+            alphanum_key = lambda key: [convert(c) for c in re.split('([0-9]+)', key)]
+            return sorted(data, key=alphanum_key)
+        folder_names = sorted_alphanumeric(folder_names)
+
+        # Check if there are any folders in recordings and initialize a folder with number 1 if not
+        try:
+            next_folder = os.path.join(recordings_path, folder_names[-1])
+        except IndexError:
+            next_folder = os.path.join(recordings_path, "1")
+            os.mkdir(next_folder)
+
+        # Check if last folder contains any items and create new folder if it does
+        if len(os.listdir(next_folder)) != 0:
+            next_folder = os.path.join(recordings_path, str(int(folder_names[-1]) + 1))
+            os.mkdir(next_folder)
+
+        return next_folder
